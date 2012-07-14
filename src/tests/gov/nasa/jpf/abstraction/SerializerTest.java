779851db4d6c20d915986e09e58116ea94a8b707
pavel <>
2012-07-14 12:22:00 -0400

    test for serializer

--- src/tests/gov/nasa/jpf/abstraction/SerializerTest.java
+++ src/tests/gov/nasa/jpf/abstraction/SerializerTest.java
@@ -35,7 +35,53 @@ public class SerializerTest
 
 		// this should be printed twice with the sign abstraction and correct serializer
 		System.out.printf("v = %s\n", Debug.getAbstractInteger(v));
+
+
+		SerializerTest st = new SerializerTest();
+		st.compute();
+		st.display();
 	}
-  
+ 
+
+	private int fx;
+	private int fy;
+	private int fz;
+	private int fv;
+	
+	public SerializerTest()
+	{
+		fx = Debug.makeAbstractInteger(1);
+		fy = Debug.makeAbstractInteger(-1);
+		fz = Debug.makeAbstractInteger(0);
+
+		fv = Debug.makeAbstractInteger(0);
+	}
+
+	public void compute()
+	{
+		boolean b = Verify.getBoolean();
+
+		if (b)
+		{
+			fv = fx + fz;
+		}
+		else
+		{
+			b = true;
+
+			fv = fy + fz;
+		}
+	}
+
+	public void display()
+	{
+		Verify.breakTransition();
+
+		// default serializer will handle this incorrectly (premature state matching)
+			// field "fv" has the same concrete value (0) in both branches
+
+		// this should be printed twice with the sign abstraction and correct serializer
+		System.out.printf("fv = %s\n", Debug.getAbstractInteger(fv));
+	}
 }
 
