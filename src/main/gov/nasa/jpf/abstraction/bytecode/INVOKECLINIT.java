afba9f2cdca1276a3912514c34aff11f7adbefdd
Jakub Daniel <daniel@d3s.mff.cuni.cz>
2013-07-04 15:12:17 +0200

    Added missing INVOKE / RETURN instructions

---
+++ src/main/gov/nasa/jpf/abstraction/bytecode/INVOKECLINIT.java
@@ -0,0 +1,42 @@
+//
+// Copyright (C) 2012 United States Government as represented by the
+// Administrator of the National Aeronautics and Space Administration
+// (NASA).  All Rights Reserved.
+// 
+// This software is distributed under the NASA Open Source Agreement
+// (NOSA), version 1.3.  The NOSA has been approved by the Open Source
+// Initiative.  See the file NOSA-1.3-JPF at the top of the distribution
+// directory tree for the complete NOSA document.
+// 
+// THE SUBJECT SOFTWARE IS PROVIDED "AS IS" WITHOUT ANY WARRANTY OF ANY
+// KIND, EITHER EXPRESSED, IMPLIED, OR STATUTORY, INCLUDING, BUT NOT
+// LIMITED TO, ANY WARRANTY THAT THE SUBJECT SOFTWARE WILL CONFORM TO
+// SPECIFICATIONS, ANY IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR
+// A PARTICULAR PURPOSE, OR FREEDOM FROM INFRINGEMENT, ANY WARRANTY THAT
+// THE SUBJECT SOFTWARE WILL BE ERROR FREE, OR ANY WARRANTY THAT
+// DOCUMENTATION, IF PROVIDED, WILL CONFORM TO THE SUBJECT SOFTWARE.
+//
+package gov.nasa.jpf.abstraction.bytecode;
+
+import gov.nasa.jpf.abstraction.predicate.PredicateAbstraction;
+import gov.nasa.jpf.vm.ClassInfo;
+import gov.nasa.jpf.vm.Instruction;
+import gov.nasa.jpf.vm.ThreadInfo;
+
+public class INVOKECLINIT extends gov.nasa.jpf.jvm.bytecode.INVOKECLINIT {
+
+	public INVOKECLINIT(ClassInfo info) {
+		super(info);
+	}
+	
+	@Override
+	public Instruction execute(ThreadInfo ti) {
+		Instruction ret = super.execute(ti);
+		
+		if (ret == this || ret != ti.getPC()) return ret;
+		
+		PredicateAbstraction.processMethodCall();
+
+		return ret;
+	}
+}
