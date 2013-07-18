d1475c22ec9f64630c02c09e2ea52d95587c0732
Jakub Daniel <daniel@d3s.mff.cuni.cz>
2013-07-18 12:17:59 +0200

    New example

---
+++ src/examples/statics/Statics.java
@@ -0,0 +1,17 @@
+package statics;
+
+class A {
+	int x;
+}
+
+public class Statics {
+
+	static int x = 1;
+	static A a = new A();
+	
+	public static void main(String[] args) {
+		statics.Statics.x = 10;
+		statics.Statics.a.x = x;
+	}
+
+}
