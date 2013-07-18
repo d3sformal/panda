258ff81f9f4b8fc6d79719408d19fc0f865b19c7
Jakub Daniel <daniel@d3s.mff.cuni.cz>
2013-07-18 09:17:39 +0200

    Redundant imports

--- src/main/gov/nasa/jpf/abstraction/common/AccessPath.java
+++ src/main/gov/nasa/jpf/abstraction/common/AccessPath.java
@@ -8,8 +8,6 @@ import gov.nasa.jpf.abstraction.common.impl.PredicatesFunctionStringifier;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class AccessPath extends Expression implements Cloneable {
 	public static enum NotationPolicy {
