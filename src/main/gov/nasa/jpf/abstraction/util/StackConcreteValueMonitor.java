52d3cfe77a774c77a978df216c739f05a1c3daa8
Jakub Daniel <daniel@d3s.mff.cuni.cz>
2013-09-12 12:32:07 +0200

    Listener for int values in stack frame

---
+++ src/main/gov/nasa/jpf/abstraction/util/StackConcreteValueMonitor.java
@@ -0,0 +1,36 @@
+package gov.nasa.jpf.abstraction.util;
+
+import gov.nasa.jpf.ListenerAdapter;
+import gov.nasa.jpf.vm.Instruction;
+import gov.nasa.jpf.vm.LocalVarInfo;
+import gov.nasa.jpf.vm.ThreadInfo;
+import gov.nasa.jpf.vm.StackFrame;
+import gov.nasa.jpf.vm.VM;
+
+public class StackConcreteValueMonitor extends ListenerAdapter {
+	
+	@Override
+	public void executeInstruction(VM vm, ThreadInfo curTh, Instruction insn) {
+		if (RunDetector.isRunning()) {
+			StackFrame sf = curTh.getTopFrame();
+			inspect(sf);
+		}
+	}
+
+	public static void inspect(StackFrame sf) {
+		//NOT HANDLING LONGS...
+		System.out.println("--CONCRETE VALUES --");
+		for (int i = 0; i <= (sf.getTopPos() - sf.getLocalVariableCount()); ++i) {
+			System.out.println(i + ": " + sf.peek(i));
+		}
+		System.out.println("--LOCAL VARS --");
+		for (int i = 0; i < sf.getLocalVariableCount(); ++i) {
+			LocalVarInfo var = sf.getLocalVarInfo(i);
+			
+			System.out.println((var == null ? null : var.getName()) + ": " + sf.getLocalVariable(i));
+		}
+		System.out.println("--------------");
+	}
+}
+
+
