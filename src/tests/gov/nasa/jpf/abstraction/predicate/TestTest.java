7605acc7b80e6c59f057e10d60b2860d17632a5f
Jakub Daniel <daniel@d3s.mff.cuni.cz>
2014-02-06 16:19:19 +0100

    Test cleanup

---
+++ src/tests/gov/nasa/jpf/abstraction/predicate/TestTest.java
@@ -0,0 +1,20 @@
+package gov.nasa.jpf.abstraction.predicate;
+
+import org.junit.Test;
+
+public class TestTest extends BaseTest {
+    public static void main (String[] args) {
+        int x = 3;
+
+        assertConjunction("x = 3: true");
+
+        x = 4;
+
+        assertDisjunction("x = 4: unknown", "0 = 0: false");
+
+        //assertAliasing("a.b.c.d", "x");
+        //assertNumberOfPossibleValues("a.b.c.d[10].e", 8);
+
+        //...
+    }
+}
