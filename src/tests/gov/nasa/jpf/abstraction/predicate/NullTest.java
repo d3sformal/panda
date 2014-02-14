d22590e4d8991741732838fdff1448b7d50c3d69
Jakub Daniel <daniel@d3s.mff.cuni.cz>
2014-02-14 17:13:03 +0100

    Tests

--- src/tests/gov/nasa/jpf/abstraction/predicate/NullTest.java
+++ src/tests/gov/nasa/jpf/abstraction/predicate/NullTest.java
@@ -10,15 +10,18 @@ public class NullTest extends NegativeBaseTest {
     public static void main(String[] args) {
         Object[] array = new Object[2];
         int i = 0;
+        boolean b = true;
 
         array[0] = new Object();
         assertNumberOfPossibleValues("array[0]", 1);
         assert array[0] != null : "Cannot guarantee array[0] to be not null"; // Should not fail - array[0] is definitely not null
 
-        array[i] = new Object();
-        assertNumberOfPossibleValues("array[1]", 2);
-        assert array[1] != null : "Cannot guarantee array[1] to be not null"; // Should fail - cannot know what element was overwritten, array[1] still can be null
-
-        //assertConjunction("array[0] = null: false"); // Should not be provable - missing predicates
+        if (b) {
+            array[i] = new Object();
+            assertNumberOfPossibleValues("array[1]", 2);
+            assert array[1] != null : "Cannot guarantee array[1] to be not null"; // Should fail - cannot know what element was overwritten, array[1] still can be null
+        } else {
+            assertConjunction("array[0] = null: false"); // Should not be provable - missing predicates
+        }
     }
 }
---
+++ src/tests/gov/nasa/jpf/abstraction/predicate/SerializeThreadTest.java
@@ -0,0 +1,30 @@
+package gov.nasa.jpf.abstraction.predicate;
+
+public class SerializeThreadTest extends BaseTest {
+    public static void main(String[] args) throws Exception {
+        new SerializeThread().test();
+    }
+}
+
+class SerializeThread {
+    public void test() throws Exception {
+        final SerializeThread lockObject = this;
+
+        Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                synchronized (lockObject) {
+                    // MAYBE DO SOMETHING
+                }
+            }
+        });
+
+        t.start();
+
+        synchronized (lockObject) {
+            // MAYBE DO SOMETHING
+        }
+
+        t.join();
+    }
+}
