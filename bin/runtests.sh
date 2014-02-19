77a133cddbaeb01de26a82f985aa4113e56e744a
pparizek <parizek@d3s.mff.cuni.cz>
2014-02-19 10:10:13 +0100

    fixed test runner

--- bin/runtests.sh
+++ bin/runtests.sh
@@ -1,3 +1,23 @@
 #!/bin/sh
 
-java -classpath ../jpf-core/build/jpf.jar:lib/junit-4.11.jar:build/classes:build/tests:build/main:lib/antlr-4.0-complete.jar gov.nasa.jpf.abstraction.predicate.NullTestDriver
+cd src/tests
+
+common_tests=`find gov/nasa/jpf/abstraction/common -name "*.java" | xargs | tr '/' '.'`
+predabs_tests=`find gov/nasa/jpf/abstraction/predicate -name "*.java" | xargs | tr '/' '.'`
+
+java_suffix=".java"
+
+cd ../..
+
+for t in $common_tests 
+do
+	echo "Running ${t%$java_suffix}"
+	java -classpath ../jpf-core/build/jpf.jar:lib/junit-4.11.jar:lib/hamcrest-core-1.3.jar:build/classes:build/tests:build/main:lib/antlr-4.0-complete.jar org.junit.runner.JUnitCore ${t%$java_suffix}
+done
+
+for t in $predabs_tests 
+do
+	echo "Running ${t%$java_suffix}"
+	java -classpath ../jpf-core/build/jpf.jar:lib/junit-4.11.jar:lib/hamcrest-core-1.3.jar:build/classes:build/tests:build/main:lib/antlr-4.0-complete.jar org.junit.runner.JUnitCore ${t%$java_suffix}
+done
+
