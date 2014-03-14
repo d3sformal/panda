fb04b55fb5ec52ccdb742e0ab2566d2fd13eac26
pparizek <parizek@d3s.mff.cuni.cz>
2014-03-14 15:24:48 +0100

    extended test

--- src/tests/gov/nasa/jpf/abstraction/predicate/ImprovedPrecisionTest.java
+++ src/tests/gov/nasa/jpf/abstraction/predicate/ImprovedPrecisionTest.java
@@ -1,13 +1,13 @@
 package gov.nasa.jpf.abstraction.predicate;
 
-class Data {
-    int f;
-    int g;
-    int h;
-}
-
 public class ImprovedPrecisionTest extends BaseTest {
     public static void main(String[] args) {
+		testCase1();
+		testCase2();
+		testCase3();
+	}
+
+	public static void testCase1() {
         int x = 5;
 
         Data a = new Data();
@@ -26,4 +26,52 @@ public class ImprovedPrecisionTest extends BaseTest {
             assertKnownValuation("b.f > 0: true");
         }
     }
+
+	public static void testCase2() {
+		int x = 1;
+		int y = 2;
+
+		Data a = new Data();
+		a.f = x;
+
+		Data b = new Data();
+		b.f = y;
+		
+		b = a;
+
+		if (a.f > 5) {
+			assertKnownValuation("b.f > 0: true");
+		}
+	}
+
+	public static void testCase3()
+	{
+		Data a = new Data();
+		a.f = 10;
+
+		Data p,r,b;
+
+		p = a;
+
+		r = testCase3M1(p);
+
+		b = r;
+
+		if (a.f > 0) {
+			assertKnownValuation("b.f > 0: true");
+		}
+	}
+
+	private static Data testCase3M1(Data v) {
+		v.g = 0;
+		return v;
+	}
 }
+
+
+class Data {
+    int f;
+    int g;
+    int h;
+}
+
