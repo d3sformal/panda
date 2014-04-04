2f6586a5cb510ce230309b4d6312724dec87c001
Jakub Daniel <daniel@d3s.mff.cuni.cz>
2014-04-04 15:47:26 +0200

    Initial version of a benchmark MinePump

---
+++ src/examples/svcomp/minepump/MinePumpSystem/MinePump.java
@@ -0,0 +1,98 @@
+package svcomp.minepump.MinePumpSystem;
+
+public class MinePump {
+    public boolean pumpRunning = false;
+    public boolean systemActive = true;
+    public Environment env;
+
+    public MinePump(Environment env) {
+        super();
+        this.env = env;
+    }
+
+    public void timeShift() {
+        if (pumpRunning)
+            env.lowerWaterLevel();
+        if (systemActive)
+            processEnvironment();
+    }
+
+    private void  processEnvironment__wrappee__base  () {
+
+    }
+
+    private void  processEnvironment__wrappee__highWaterSensor  () {
+        if (!pumpRunning && isHighWaterLevel()) {
+            activatePump();
+            processEnvironment__wrappee__base();
+        } else {
+            processEnvironment__wrappee__base();
+        }
+    }
+
+    private void  processEnvironment__wrappee__methaneQuery  () {
+        if (pumpRunning && isLowWaterLevel()) {
+            deactivatePump();
+        } else {
+            processEnvironment__wrappee__highWaterSensor();
+        }
+    }
+
+    public void processEnvironment() {
+        if (pumpRunning && isMethaneAlarm()) {
+            deactivatePump();
+        } else {
+            processEnvironment__wrappee__methaneQuery();
+        }
+    }
+
+    private void  activatePump__wrappee__lowWaterSensor  () {
+        pumpRunning = true;
+    }
+
+    void activatePump() {
+        if (!isMethaneAlarm()) {
+            activatePump__wrappee__lowWaterSensor();
+        } else {
+            //System.out.println("Pump not activated due to methane alarm");
+        }
+    }
+
+    void deactivatePump() {
+        pumpRunning = false;
+    }
+
+    boolean isMethaneAlarm() {
+        return env.isMethaneLevelCritical();
+    }
+
+    @Override
+    public String toString() {
+        return "Pump(System:" + (systemActive?"On":"Off") + ",Pump:" + (pumpRunning?"On":"Off") +") " + env.toString();
+    }
+
+    public Environment getEnv() {
+        return env;
+    }
+
+    boolean isHighWaterLevel() {
+        return !env.isHighWaterSensorDry();
+    }
+
+    boolean isLowWaterLevel() {
+        return !env.isLowWaterSensorDry();
+    }
+
+    public void stopSystem() {
+        if (pumpRunning) {
+            deactivatePump();
+        }
+        assert !pumpRunning;
+        systemActive = false;
+    }
+
+    public void startSystem() {
+        assert !pumpRunning;
+        systemActive = true;
+    }
+}
