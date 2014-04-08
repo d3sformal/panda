a242669788a218e4fd7ef2177c83d10ff09ff477
Jakub Daniel <daniel@d3s.mff.cuni.cz>
2014-04-08 12:35:47 +0200

    Initial jpf files for selected benchmarks

--- src/examples/svcomp/loops/Eureka01FalseUnreachableLabel.java
+++ src/examples/svcomp/loops/Eureka01FalseUnreachableLabel.java
@@ -7,14 +7,19 @@ public class Eureka01FalseUnreachableLabel {
         int nodecount = unknown();
         int edgecount = unknown();
 
-        if (0 > nodecount || nodecount > 4) return;
-        if (0 > edgecount || edgecount > 19) return;
+        if (0 > nodecount || nodecount > 5) return;
+        if (0 > edgecount || edgecount > 20) return;
 
         int source = 0;
         int[] Source = new int[] {0, 4, 1, 1, 0, 0, 1, 3, 4, 4, 2, 2, 3, 0, 0, 3, 1, 2, 2, 3};
         int[] Dest = new int[] {1, 3, 4, 1, 1, 4, 3, 4, 3, 0, 0, 0, 0, 2, 3, 0, 2, 1, 0, 4};
         int[] Weight = new int[] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19};
         int[] distance = new int[5];
+
+        // Assume edges have their ends in range
+        for (int k = 0; k < edgecount; ++k) {
+            if (Source[k] >= nodecount || Dest[k] >= nodecount) return;
+        }
 
         for (int i = 0; i < nodecount; i++) {
             if (i == source) {
