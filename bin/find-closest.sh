3dc28ea0eb8066fc25a66cb29cd48e4581647dd5
Jakub Daniel <daniel@d3s.mff.cuni.cz>
2014-04-16 17:38:12 +0200

    State difference analysis tool

--- bin/find-closest.sh
+++ bin/find-closest.sh
@@ -1,48 +1,137 @@
 #!/bin/sh
 
-METHOD=${1}
+if [ ${THREAD} ]
+then
+    THREADSELECTIONINIT='
+            head = 0;
+            selected = 0;'
+
+    THREADSELECTION='
+        /^\W+id: [0-9]+$/ {
+            if (head && $2 == '${THREAD}') {
+                selected = 1;
+            }
+        }
+
+        {
+            head = 0;
+        }
+
+        /=+ Thread =+/ {
+            head = 1;
+            selected = 0;
+        }'
+
+    MASK='
+        BEGIN {'${THREADSELECTIONINIT}'
+        }
+'${THREADSELECTION}'
+        {
+            if (selected) {
+                print $0;
+            }
+        }'
+else
+    THREADSELECTIONINIT='
+            selected = 1;'
+    THREADSELECTION=''
+    MASK='{print $0}'
+fi
 
 if [ ${METHOD} ]
 then
     ESCAPED=$(echo ${METHOD} | sed 's/\./\\./g')
-    FILTER='BEGIN {matches = 0; top = 0} /frame \[depth = 0\]/ {top = 1} /frame \[depth = 1\]/ {top = 0} /pc:/ {if ($2 ~ /^'${ESCAPED}'/) matches = 1} END {if (matches) print FILENAME}'
+    FILTER='
+        BEGIN {'${THREADSELECTIONINIT}'
+            matches = 0;
+            top = 0;
+        }
+'${THREADSELECTION}'
+
+        /frame \[depth = 0\]/ {
+            top = 1;
+        }
+
+        /frame \[depth = 1\]/ {
+            top = 0;
+        }
+
+        /pc:/ {
+            if (selected && top && $2 ~ /^'${ESCAPED}'/) {
+                matches = 1;
+            }
+        }
+
+        END {
+            if (matches) {
+                print FILENAME;
+            }
+        }'
 else
     FILTER='END {print FILENAME}'
 fi
 
-LIMIT=500
+OFFSET=50
+LIMIT=50
+PREVIEW=100
 
 # Collect states where a thread entered the target method
-STATES=$(find tmp -name "*.0" -exec awk "${FILTER}" {} \; | sort -t. -k 2n | head -n ${LIMIT})
+STATES=$(find tmp -name "*.0" -exec awk "${FILTER}" {} \; | sort -t. -k 2n | tail -n +${OFFSET} | head -n ${LIMIT})
+
+if [ -z "${STATES}" ]
+then
+    COUNT=0
+else
+    COUNT=$(echo "${STATES}" | wc -l)
+fi
 
 # Statistics
-echo "Exploring " $(wc -l <<END
-${STATES}
-END
-) " states."
+echo "Exploring " ${COUNT} " states."
+
+echo "${STATES}" | head -n ${PREVIEW}
+if [ ${PREVIEW} -lt ${COUNT} ]
+then
+    echo "..."
+fi
+
+DIFFS=$(
 
 for s1 in ${STATES}
 do
     # Compare only to states following states
-    STATES=$(tail -n +2 <<END
-${STATES}
-END
-)
+    STATES=$(echo "${STATES}" | tail -n +2)
 
     for s2 in ${STATES}
     do
         DIFF=$(diff ${s1} ${s2})
 
         # Skip states where program counters differ
-        if ! grep -q pc: <<END
-${DIFF}
-END
+        if ! echo "${DIFF}" | grep -q pc:
         then
             # Count number of different lines for each pair
-            printf "%s %s %i\n" ${s1} ${s2} $(sed -n '/^[<>]/p' <<END | wc -l
-${DIFF}
-END
-)
+            printf "%s %s %i\n" ${s1} ${s2} $(echo "${DIFF}" | sed -n '/^[<>]/p' | wc -l)
         fi
     done
-done | sort -t. -k 2n | sort -s -k 3n
+done | sort -s -k 3n
+
+)
+
+echo
+echo "Found " $(echo "${DIFFS}" | wc -l) " interesting differences."
+echo
+
+if [ -t 1 ]
+then
+    (
+        for pair in $(echo "${DIFFS}" | sed 's/ [0-9]*$//;s/ /:/')
+        do
+            s1=$(echo ${pair} | cut -d":" -f1)
+            s2=$(echo ${pair} | cut -d":" -f2)
+
+            sh -x -c "diff -C3 ${s1} ${s2}"
+            read
+        done
+    )
+else
+    echo "${DIFFS}"
+fi
