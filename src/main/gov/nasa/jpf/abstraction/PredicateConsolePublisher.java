35bcc063de6088b5b5a9af1bb8796b57f1ac8596
Pavel Parizek <parizek@d3s.mff.cuni.cz>
2014-09-19 14:46:03 +0200

    skipping initialization

--- src/main/gov/nasa/jpf/abstraction/PredicateConsolePublisher.java
+++ src/main/gov/nasa/jpf/abstraction/PredicateConsolePublisher.java
@@ -9,6 +9,7 @@ import gov.nasa.jpf.util.Left;
 import gov.nasa.jpf.vm.ClassInfo;
 import gov.nasa.jpf.vm.Instruction;
 import gov.nasa.jpf.vm.MethodInfo;
+import gov.nasa.jpf.vm.ThreadInfo;
 import gov.nasa.jpf.vm.Path;
 import gov.nasa.jpf.vm.Step;
 import gov.nasa.jpf.vm.Transition;
@@ -18,6 +19,9 @@ import gov.nasa.jpf.abstraction.state.State;
 import gov.nasa.jpf.abstraction.state.Trace;
 
 public class PredicateConsolePublisher extends ConsolePublisher {
+
+    // whether to skip static init instructions when printing code
+    private boolean skip;
 
     public PredicateConsolePublisher(Config conf, Reporter reporter) {
         super(conf, reporter);
@@ -36,10 +40,16 @@ public class PredicateConsolePublisher extends ConsolePublisher {
             return; // nothing to publish
         }
 
+        skip = true;
+
         publishTopicStart("trace " + reporter.getCurrentErrorId());
 
         Trace trace = PredicateAbstraction.getInstance().getTrace();
         int progressAlongTrace = 1;
+
+        // get main thread and method
+        ThreadInfo tiMain = path.get(0).getThreadInfo();
+        MethodInfo miMain = tiMain.getEntryMethod();
 
         for (Transition t : path) {
             out.print("------------------------------------------------------ ");
@@ -66,16 +76,43 @@ public class PredicateConsolePublisher extends ConsolePublisher {
                 int nNoSrc = 0;
 
                 for (Step s : t) {
+                    Instruction insn = s.getInstruction();
+                    pc = s.getInstruction().getPosition();
+
+                    // start printing code in the main method
+                    if (insn.getMethodInfo() == miMain) skip = false;
+
+                    if (skip) continue;
+
+                    if (showMethod) {
+                        MethodInfo mi = insn.getMethodInfo();
+
+                        if (mi != lastMi) {
+                            ClassInfo mci = mi.getClassInfo();
+
+                            out.print("  ");
+
+                            if (mci != null) {
+                                out.print(mci.getName());
+                                out.print(".");
+                            }
+
+                            out.println(mi.getUniqueName());
+
+                            lastMi = mi;
+                        }
+                    }
+
                     if (showSource) {
                         String line = s.getLineString();
 
                         if (line != null) {
                             if (!line.equals(lastLine)) {
                                 if (nNoSrc > 0) {
-                                    out.println("      [" + nNoSrc + " insn w/o sources]");
+                                    out.println("        [" + nNoSrc + " insn w/o sources]");
                                 }
 
-                                out.print("  ");
+                                out.print("    ");
 
                                 if (showLocation) {
                                     out.print(Left.format(s.getLocationString(),30));
@@ -92,30 +129,7 @@ public class PredicateConsolePublisher extends ConsolePublisher {
                         lastLine = line;
                     }
 
-                    pc = s.getInstruction().getPosition();
-
-                    if (showCode) {
-                        Instruction insn = s.getInstruction();
-
-                        if (showMethod) {
-                            MethodInfo mi = insn.getMethodInfo();
-
-                            if (mi != lastMi) {
-                                ClassInfo mci = mi.getClassInfo();
-
-                                out.print("    ");
-
-                                if (mci != null) {
-                                    out.print(mci.getName());
-                                    out.print(".");
-                                }
-
-                                out.println(mi.getUniqueName());
-
-                                lastMi = mi;
-                            }
-                        }
-
+                    if (showCode) {                        
                         out.print("      ");
                         out.println(insn);
                     }
