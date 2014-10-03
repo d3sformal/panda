2a0b605f27c404ee5c00d82e7e2572e8536f0af1
Jakub Daniel <daniel@d3s.mff.cuni.cz>
2014-10-03 10:53:32 +0200

    Test refinement

--- src/tests/gov/nasa/jpf/abstraction/statematch/UnknownTest.java
+++ src/tests/gov/nasa/jpf/abstraction/statematch/UnknownTest.java
@@ -11,21 +11,39 @@ public class UnknownTest extends StateMatchingTest {
         //config.add("+listener+=,gov.nasa.jpf.abstraction.util.InstructionTracker");
         //config.add("+listener+=,gov.nasa.jpf.abstraction.util.PredicateValuationMonitor");
         //config.add("+listener+=,gov.nasa.jpf.abstraction.util.CounterexampleListener");
-        config.add("+vm.storage.class="); // Disable state matching
+        config.add("+vm.storage.class="); // Disable state matching, to avoid matching abstract states with different concrete values
     }
 
-    @Test
+    //@Test
     public static void test1() {
         int i = Debug.unknownInt();
 
+        // Without matching already explored abstract traces
+        // 1. Comes with 0
+        // 2. Generates unknown = 1 ... to get to now-disabled then-branch
+        // 3. Comes with 1
+        // 4. Generates unknown = -1 ... to get to now-disabled else-branch
+        // 5. Comes with -1
+        // 6. Cannot generate more models for i
         if (i == 1) {
         }
 
         assertRevisitedAtLeast(1);
     }
 
-    //@Test
+    @Test
     public static void test2() {
+        int i = Debug.unknownInt();
+
+        // Test with the special value 0 which is the default
+        if (i != 0) {
+        }
+
+        assertRevisitedAtLeast(1);
+    }
+
+    //@Test
+    public static void test3() {
         int i = Debug.unknownInt(); // Should register choice (starting with for example 0) and mark the constant as NONDET
 
         int j = i + 2;
