10969bc8f97f35ed1858e0a1648ae8dab2ffc9ad
Jakub Daniel <daniel@d3s.mff.cuni.cz>
2014-11-06 10:47:47 +0100

    Minor

--- src/main/gov/nasa/jpf/abstraction/AddEqualityRefinementHeuristic.java
+++ src/main/gov/nasa/jpf/abstraction/AddEqualityRefinementHeuristic.java
@@ -2,9 +2,11 @@ package gov.nasa.jpf.abstraction;
 
 import gov.nasa.jpf.vm.MethodInfo;
 
+import gov.nasa.jpf.abstraction.common.Constant;
 import gov.nasa.jpf.abstraction.common.Equals;
 import gov.nasa.jpf.abstraction.common.LessThan;
 import gov.nasa.jpf.abstraction.common.Predicate;
+import gov.nasa.jpf.abstraction.common.access.AccessExpression;
 import gov.nasa.jpf.abstraction.state.SystemPredicateValuation;
 
 public class AddEqualityRefinementHeuristic extends RefinementHeuristic {
@@ -18,9 +20,13 @@ public class AddEqualityRefinementHeuristic extends RefinementHeuristic {
 
         if (interpolant instanceof LessThan) {
             LessThan lt = (LessThan) interpolant;
-            Predicate eq = Equals.create(lt.a, lt.b);
 
-            refined |= super.refineAtomic(eq, m, fromPC, toPC);
+            if (lt.a instanceof AccessExpression && lt.b instanceof Constant ||
+                lt.b instanceof AccessExpression && lt.a instanceof Constant) {
+                Predicate eq = Equals.create(lt.a, lt.b);
+
+                refined |= super.refineAtomic(eq, m, fromPC, toPC);
+            }
         }
 
         refined |= super.refineAtomic(interpolant, m, fromPC, toPC);
