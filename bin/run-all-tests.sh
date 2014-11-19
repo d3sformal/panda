fa18e567480b1b246678b6f0e80f6f4aacfd7b1e
Jakub Daniel <daniel@d3s.mff.cuni.cz>
2014-11-19 16:58:47 +0100

    Test running

--- bin/run-all-tests.sh
+++ bin/run-all-tests.sh
@@ -3,7 +3,7 @@
 cd src/tests
 
 common_tests=`find gov/nasa/jpf/abstraction/common -name "*.java" | xargs | tr '/' '.'`
-predabs_tests=`find gov/nasa/jpf/abstraction -name "*.java" | xargs | tr '/' '.'`
+predabs_tests=`find gov/nasa/jpf/abstraction -mindepth 1 -maxdepth 1 -name "*.java" | xargs | tr '/' '.'`
 
 cd ../..
 
@@ -12,12 +12,12 @@ java_suffix=".java"
 for t in $common_tests 
 do
 	echo "Running ${t%$java_suffix}"
-	java -classpath ../jpf-core/build/jpf.jar:lib/junit-4.11.jar:lib/hamcrest-core-1.3.jar:build/classes:build/tests:build/main:lib/antlr-4.0-complete.jar org.junit.runner.JUnitCore ${t%$java_suffix}
+	java -cp lib/jpf-core/build/jpf.jar:lib/junit-4.11.jar:lib/hamcrest-core-1.3.jar:lib/antlr-4.0-complete.jar:build/annotations/:build/classes/:build/main/:build/peers/:build/tests/ org.junit.runner.JUnitCore ${t%$java_suffix}
 done
 
 for t in $predabs_tests 
 do
 	echo "Running ${t%$java_suffix}"
-	java -classpath ../jpf-core/build/jpf.jar:lib/junit-4.11.jar:lib/hamcrest-core-1.3.jar:build/classes:build/tests:build/main:lib/antlr-4.0-complete.jar org.junit.runner.JUnitCore ${t%$java_suffix}
+	bin/run-test.sh ${t%$java_suffix}
 done
 
--- bin/run-test.sh
+++ bin/run-test.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
-java -cp lib/jpf-core/build/jpf.jar:lib/junit-4.11.jar:lib/antlr-4.0-complete.jar:build/annotations/:build/classes/:build/main/:build/peers/:build/tests/ gov.nasa.jpf.abstraction.util.TestRunner $1
+CMD="java -cp lib/jpf-core/build/jpf.jar:lib/junit-4.11.jar:lib/antlr-4.0-complete.jar:build/annotations/:build/classes/:build/main/:build/peers/:build/tests/ gov.nasa.jpf.abstraction.util.TestRunner"
+
+if [ $1 = "-q" ]
+then
+    $CMD $2 >/dev/null 2>&1
+else
+    $CMD $1
+fi
