a06d351174b82e8f855c4436d3498523b5838ae7
Jakub Daniel <daniel@d3s.mff.cuni.cz>
2015-01-30 14:04:27 +0100

    Benchmarks

---
+++ bin/run-cav15-full-backtrack.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+bin/run.sh src/benchmarks/svcomp/loops/ArrayTrueUnreachableLabelAdjustingFullBacktrack.jpf
+bin/run.sh src/benchmarks/svcomp/loops/Eureka01TrueUnreachableLabelAdjustingFullBacktrack.jpf
+bin/run.sh src/benchmarks/svcomp/loops/TREX03TrueUnreachableLabelAdjustingFullBacktrack.jpf
+bin/run.sh src/benchmarks/svcomp/loops/InvertStringTrueUnreachableLabelAdjustingFullBacktrack.jpf
+bin/run.sh src/benchmarks/svcomp/arrays/PasswordTestAdjustingFullBacktrack.jpf
+bin/run.sh src/benchmarks/svcomp/arrays/ReverseArrayTestAdjustingFullBacktrack.jpf
+bin/run.sh src/benchmarks/svcomp/arrays/TwoIndicesTestAdjustingFullBacktrack.jpf
