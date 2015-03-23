112cea097520e4d5488f6a4f6a1bd92d90d1e731
Jakub Daniel <daniel@d3s.mff.cuni.cz>
2015-03-23 17:12:15 +0100

    Comments

--- src/peers/gov/nasa/jpf/abstraction/JPF_gov_nasa_jpf_abstraction_Verifier.java
+++ src/peers/gov/nasa/jpf/abstraction/JPF_gov_nasa_jpf_abstraction_Verifier.java
@@ -49,20 +49,15 @@ public class JPF_gov_nasa_jpf_abstraction_Verifier extends NativePeer {
             // at some point we reach a feasible branch which is not enabled under current concrete execution
             // we generate a model of all unknown values
             // we backtrack and try different choices
-            //
-            // what happens:
-            // suppose the only enabling pair of unknown values is u1=1, u2=1
-            // we need to backtrack back to u1 (choice generator appearing first)
-            // at that point we have dropped u2
-            // we reintroduce a new generator for u2 which does not have the correct value
-            //
-            // question: why the hell does it not suffice to wait for the exploration to reach the disabled branch and at that point come up with the new value for u2
-            // if we are unlucky it might come up with a different model (even for u1, which would cause further backtracking)
-            // -> divergence
             String name = ((Root) PredicateAbstraction.getInstance().getSSAManager().getSymbolIncarnation(Unknown.create(ti.getPC(), cg), 0)).getName();
             Map<String, Unknown> unknowns = PredicateAbstraction.getInstance().getUnknowns();
             Integer[] prevValues = new Integer[0];
             List<TraceFormula> prevTraces = null;
+
+            // Check that exact values (the current values during execution) of previous unknowns match the enabling constraints (the values discovered together with this model)
+            //   value(u1) = v1
+            //   value(u2) = u2
+            //   ...
             List<Map<String, Integer>> prevConditions = null;
 
             if (unknowns.containsKey(name)) {
