f9ac017cd7b6a527f0b765c433f3f0d341f9008d
Jakub Daniel <daniel@d3s.mff.cuni.cz>
2015-07-13 16:22:29 +0200

    Extend trace formula with violating assumption about logical operands whenever they cannot be proven to belong to {0, 1}. Helps derive predicates and avoid spurious traces.

--- src/main/gov/nasa/jpf/abstraction/bytecode/LogicalOperandChecker.java
+++ src/main/gov/nasa/jpf/abstraction/bytecode/LogicalOperandChecker.java
@@ -1,11 +1,15 @@
 package gov.nasa.jpf.abstraction.bytecode;
 
+import gov.nasa.jpf.vm.Instruction;
+import gov.nasa.jpf.vm.ThreadInfo;
+
 import gov.nasa.jpf.abstraction.PredicateAbstraction;
 import gov.nasa.jpf.abstraction.common.Conjunction;
 import gov.nasa.jpf.abstraction.common.Constant;
 import gov.nasa.jpf.abstraction.common.Disjunction;
 import gov.nasa.jpf.abstraction.common.Equals;
 import gov.nasa.jpf.abstraction.common.Expression;
+import gov.nasa.jpf.abstraction.common.Negation;
 import gov.nasa.jpf.abstraction.common.Predicate;
 import gov.nasa.jpf.abstraction.state.TruthValue;
 import gov.nasa.jpf.abstraction.util.RunDetector;
@@ -27,6 +31,9 @@ public class LogicalOperandChecker {
             TruthValue value = PredicateAbstraction.getInstance().processBranchingCondition(lastPC, inSupportedDomain);
 
             if (value != TruthValue.TRUE) {
+                Instruction pc = ThreadInfo.getCurrentThread().getPC();
+                PredicateAbstraction.getInstance().extendTraceFormulaWithConstraint(Negation.create(inSupportedDomain), pc.getMethodInfo(), pc.getPosition());
+
                 throw new IllegalArgumentException("logical and bitwise operations over values other than {0, 1} are not supported");
             }
         }
