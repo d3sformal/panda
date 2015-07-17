f41e5ab303407297352d1341fbc86add27d21697
Jakub Daniel <daniel@d3s.mff.cuni.cz>
2015-07-17 16:27:46 +0200

    Fix script + add new benchmarks

--- bin/run-hvc15.sh
+++ bin/run-hvc15.sh
@@ -8,8 +8,8 @@ echo SAFE
 
 bin/run.sh $CONFIG src/benchmarks/svcomp/loops/ArrayTrueUnreachableLabelPruning.jpf 2>&1 | tee output/array-pruning
 echo
-#bin/run.sh $CONFIG src/benchmarks/svcomp/loops/Eureka01TrueUnreachableLabelPruning.jpf 2>&1 | tee output/eureka-pruning
-#echo
+bin/run.sh $CONFIG src/benchmarks/svcomp/loops/Eureka01TrueUnreachableLabelPruning.jpf 2>&1 | tee output/eureka-pruning
+echo
 bin/run.sh $CONFIG src/benchmarks/svcomp/loops/TREX03TrueUnreachableLabelPruning.jpf 2>&1 | tee output/trex-pruning
 echo
 bin/run.sh $CONFIG src/benchmarks/svcomp/loops/InvertStringTrueUnreachableLabelPruning.jpf 2>&1 | tee output/invert-string-pruning
@@ -20,10 +20,14 @@ bin/run.sh $CONFIG src/benchmarks/svcomp/arrays/ReverseArrayPruning.jpf 2>&1 | t
 echo
 bin/run.sh $CONFIG src/benchmarks/svcomp/arrays/TwoIndicesPruning.jpf 2>&1 | tee output/two-indices-pruning
 echo
-#bin/run.sh $CONFIG src/benchmarks/dataflow/DataFlowAnalysisPruning.jpf 2>&1 | tee output/dataflow-pruning
-#echo
+bin/run.sh $CONFIG src/benchmarks/dataflow/DataFlowAnalysisPruning.jpf 2>&1 | tee output/dataflow-pruning
+echo
 bin/run.sh $CONFIG src/benchmarks/cycling/CyclingRacePruning.jpf 2>&1 | tee output/cycling-pruning
 echo
-#bin/run.sh $CONFIG src/benchmarks/image/ImagePruning.jpf 2>&1 | tee output/image-pruning
-#echo
+bin/run.sh $CONFIG src/benchmarks/image/ImagePruning.jpf 2>&1 | tee output/image-pruning
+echo
 bin/run.sh $CONFIG src/benchmarks/scheduler/SchedulerPruning.jpf 2>&1 | tee output/scheduler-pruning
+echo
+bin/run.sh $CONFIG src/benchmarks/alarmclock/SerialAlarmClockPruning.jpf 2>&1 | tee output/alarmclock-pruning
+echo
+bin/run.sh $CONFIG src/benchmarks/prodcons/SerialProducerConsumerPruning.jpf 2>&1 | tee output/prodcons-pruning
